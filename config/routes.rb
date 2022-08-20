9c2bfc83ecf9f1e1cfb6f159e4ab07195f9c065f
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-08-20 08:21:27 +0000

    add controller items

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   end
 
   namespace :public do
+    resources :items, only: [:index, :show]
     resources :cart_items, only: [:index, :create, :update, :destroy]
     delete 'destroy_all' => 'cart_items#destroy_all'
   end
