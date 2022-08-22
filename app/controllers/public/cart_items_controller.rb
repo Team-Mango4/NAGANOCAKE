34fb397188a621429ec803ab11229276a1635dc7
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-08-22 09:22:52 +0000

    途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,21 +6,36 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to public_items_path
+    if cart_items.find_by(item_id: )
+      @cart_item_amount = cart_items.find_by(id: item_id).amount.to_i
+      @cart_item.amount.to_i += params[:amount].to_i
+      @cart_item.update(cart_item_params)
+      #@cart_item = current_customer.cart_items.build(cart_item_params)
+      #@cart_items = current_customer.cart_items.all
+      #@cart_items.each do |cart_item|
+        #cart_item.amount.to_i += @cart_item.item_id
+    else
+
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+      redirect_to public_items_path
+    end
+
   end
 
   def update
   end
 
   def destroy
-    @cart_item = 
-    
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to public_cart_items_path
   end
 
   def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to public_cart_items_path
   end
 
 
