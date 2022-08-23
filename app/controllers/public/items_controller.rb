e2bc693d26b376d61132cecea66e53f3ddb46c8e
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-08-23 00:54:18 +0000

    lyout

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,9 +2,11 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
